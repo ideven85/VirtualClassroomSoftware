1f757b7eddae1f99bb732c424aee3a4ca20700f6
Deven Kalra <deven@Macbook.local>
2024-05-03 12:48:52 +0530

    Started Working on LLM

---
+++ llm_module/src/test/java/com/cleo/llm_module/LlmModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleo.llm_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
